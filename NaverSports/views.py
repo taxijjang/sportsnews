ac108e76844c02c73a548a18f8b6186996164856
unknown <gw9122@naver.com>
2020-05-18 19:43:58 +0900

    heroku

--- NaverSports/views.py
+++ NaverSports/views.py
@@ -3,7 +3,7 @@ from rest_framework import viewsets
 
 from .serializer import NaverSportsSerializer
 from .models import NaverSports
-from . import task
+#from . import task
 
 from rest_framework.decorators import api_view
 
